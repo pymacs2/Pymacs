8b29be7a26ec10bcbe04b6e8a29704f9beebe55b
Dennis Gentry <dennis.gentry@gmail.com>
2017-06-13 16:30:29 -0700

    This gets pyopenssl closer on RasPi, but not sufficient.

--- install-pymacs.sh
+++ install-pymacs.sh
@@ -15,6 +15,15 @@ else
     echo "Doing a user install of rope and ropemacs."
 fi
 
+# Older rpi might be "armv6l"
+if [[ `uname` == 'Linux' && `uname -m` == "armv7l" ]]; then
+    echo "Apparently on Raspbian, pyopenssl needs python-dev and libffi"
+    echo "in order to compile and install.  It mostly seems to work without"
+    echo "them anyway, but if you want to sudo, here you go."
+    echo " "
+    sudo apt-get install python-dev libffi-dev
+fi
+
 python -m pip install $USERFLAG --upgrade pyopenssl
 python -m pip install rope ropemacs $USERFLAG python
 
