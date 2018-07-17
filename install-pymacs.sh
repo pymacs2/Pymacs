fb5253eafaf633f5b52c4c8a3c41010c43c41415
Dennis Gentry <dennis.gentry@gmail.com>
2018-07-16 21:19:11 -0700

    Attempt installs on multiple Unixes.

--- install-pymacs.sh
+++ install-pymacs.sh
@@ -15,12 +15,30 @@ else
     echo "Doing a user install of rope and ropemacs."
 fi
 
+# Find the install command
+# We'll look for dnf (Red Hat/Fedora), apt-get (Debian/Ubuntu), and brew (MacOS)
+CHECK_CMD=$(which dnf 2> /dev/null)
+if [[ -x "${CHECK_CMD}" ]]; then
+    INSTALL_CMD="sudo ${CHECK_CMD} install -y"
+fi
+CHECK_CMD=$(which apt-get 2> /dev/null)
+if [[ -x "${CHECK_CMD}" ]]; then
+    INSTALL_CMD="sudo ${INSTALL_CMD} install -y"
+fi
+CHECK_CMD=$(which brew 2> /dev/null)
+if [[ -x "${CHECK_CMD}" ]]; then
+    INSTALL_CMD="${CHECK_CMD} install"
+fi
+
+echo "Using ${INSTALL_CMD} to install."
+
 if [[ `uname` == 'Linux' ]]; then
     echo "Apparently on Raspbian, Ubuntu, and Debian, pyopenssl needs python-dev and libffi"
     echo "in order to compile and install.  It mostly seems to work without"
     echo "them anyway, but if you want to sudo, here you go."
     echo " "
-    sudo apt-get install python-pip libssl-dev libffi-dev python-dev
+    echo "${INSTALL_CMD} python-pip libssl-dev libffi-dev python-dev"
+    ${INSTALL_CMD} python-pip libssl-dev libffi-dev python-dev
     sudo pip install -U setuptools
 fi
 
@@ -30,7 +48,7 @@ elif [[ -d ~/.emacs.d ]]; then
     USER_LISP_DIR="$HOME/.emacs.d"
 else
     echo "Couldn't guess a good place to put pymacs.el."
-    echo "You should manually copy it from $HOME/pymacs.el."
+    echo "You should manually copy it from $HOME/pymacs.el to your emacs init directory."
     USER_LISP_DIR="$HOME"
 fi
 
