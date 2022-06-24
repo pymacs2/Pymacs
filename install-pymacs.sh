a90a217bc4a0979e01b78b10ed74519a2b4f85ad
Dennis Gentry <dennis.gentry@gmail.com>
2022-06-24 09:56:47 -0700

    Install python3 explicitly, use "msg."

--- install-pymacs.sh
+++ install-pymacs.sh
@@ -3,8 +3,21 @@
 # Install pymacs stuff that emacs needs (outside emacs), into the current
 # virtual environment, if there is one.
 
+# Variables for colors/contrast
+txtbld=$(tput bold)             # Bold
+bldblu=${txtbld}$(tput setaf 4) #  blue
+txtrst=$(tput sgr0)             # Reset
+
+myname='install-pymacs'
+# Provide our messages with our name, and contrast to the other
+# install messages that will be going by
+msg() {
+    echo "$bldblu${myname}: $*${txtrst}"
+}
+
+
 if [[ -e "$VIRTUAL_ENV" ]]; then
-    echo "Installing (rope, ropemacs, and) pymacs into virtual environment $VIRTUAL_ENV"
+    msg "Installing (rope, ropemacs, and) pymacs into virtual environment $VIRTUAL_ENV"
     SRC="$VIRTUAL_ENV/src"
     USERFLAG=
     SUDO=
@@ -12,7 +25,7 @@ else
     SRC=src
     USERFLAG=--user
     SUDO=sudo
-    echo "Doing a user install of rope and ropemacs."
+    msg "Doing a user install of rope and ropemacs."
 fi
 
 # Find the install command
@@ -30,16 +43,19 @@ if [[ -x "${CHECK_CMD}" ]]; then
     INSTALL_CMD="${CHECK_CMD} install"
 fi
 
-echo "Using ${INSTALL_CMD} to install."
+msg "Using ${INSTALL_CMD} to install."
 
 if [[ `uname` == 'Linux' ]]; then
-    echo "Apparently on Raspbian, Ubuntu, and Debian, pyopenssl needs python-dev and libffi"
-    echo "in order to compile and install.  It mostly seems to work without"
-    echo "them anyway, but if you want to sudo, here you go."
-    echo " "
-    echo "${INSTALL_CMD} python3-pip libssl-dev libffi-dev python-dev"
-    ${INSTALL_CMD} python3-pip libssl-dev libffi-dev python-dev
+    msg "Apparently on Raspbian, Ubuntu, and Debian, pyopenssl needs python-dev and libffi"
+    msg "in order to compile and install.  It mostly seems to work without"
+    msg "them anyway, but if you want to sudo, here you go."
+    msg " "
+    PKGS='python3-pip python3-dev'
+    msg "${INSTALL_CMD}  ${PKGS}"
+    ${INSTALL_CMD} ${PKGS}
     sudo pip install -U setuptools
+    PKGS='libssl-dev libffi-dev'
+    msg "${INSTALL_CMD} ${PKGS}"
 fi
 
 if [[ -d ~/.emacs.d/lisp ]]; then
@@ -47,25 +63,28 @@ if [[ -d ~/.emacs.d/lisp ]]; then
 elif [[ -d ~/.emacs.d ]]; then
     USER_LISP_DIR="$HOME/.emacs.d"
 else
-    echo "Couldn't guess a good place to put pymacs.el."
-    echo "You should manually copy it from $HOME/pymacs.el to your emacs init directory."
+    msg "Couldn't guess a good place to put pymacs.el."
+    msg "You should manually copy it from $HOME/pymacs.el to your emacs init directory."
     USER_LISP_DIR="$HOME"
 fi
 
 python -m pip install $USERFLAG --upgrade pyopenssl
-python -m pip install rope ropemacs $USERFLAG python
+python -m pip install rope ropemacs
 
 python -m pip install --editable "git+https://github.com/dgentry/Pymacs.git#egg=Pymacs"
 pushd "$SRC"
 cd pymacs/
 make check
 ${SUDO} make install
-echo " "
-echo "Copying pymacs.el to $USER_LISP_DIR"
+msg " "
+msg "Copying pymacs.el to $USER_LISP_DIR"
 cp pymacs.el $USER_LISP_DIR
-echo " "
+msg " "
 popd
-echo "Finished in:" $(pwd)
-cd
-echo "Final check:"
+msg "Final check:"
 python -c 'import Pymacs'
+if [ $? ]; then
+    msg "Success."
+else
+    msg "Some kind of error: Status was $?"
+fi
